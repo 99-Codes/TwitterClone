22bc00eec47cfca8f66e2801e258597a952629a8
Admin <adam.m.greenwood@outlook.com>
2020-04-24 22:50:02 +0100

    Completed nav drawer design
    implemented logout feature in nav drawer

--- app/src/main/java/com/example/ac_twitterclone/SocialMediaActivity.java
+++ app/src/main/java/com/example/ac_twitterclone/SocialMediaActivity.java
@@ -2,29 +2,58 @@ package com.example.ac_twitterclone;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.parse.ParseUser;
 
-public class SocialMediaActivity extends AppCompatActivity {
+public class SocialMediaActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    Button btnLogout;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
-        btnLogout = findViewById(R.id.btnLogout);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
     }
 
-    public void onClickLogout (View v) {
-        Intent intent = new Intent(SocialMediaActivity.this, Login.class);
-        startActivity(intent);
-        ParseUser.logOut();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_logout:
+                Intent intent = new Intent(SocialMediaActivity.this, Login.class);
+                startActivity(intent);
+                ParseUser.logOut();
+                break;
+
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
